b6bff3fa3810556f1ff6e67c46f2d75896b902fa
pmeinhardt <mnhrdt@gmail.com>
2023-02-15 16:19:24 +0100

    Add command-line arguments to `ql` command 🦻

--- ql.swift
+++ ql.swift
@@ -3,11 +3,24 @@
 import Cocoa
 import QuickLookUI
 
-class PreviewItem : NSObject, QLPreviewItem {
-    private var url: URL!
+class PreviewItem: NSObject, QLPreviewItem {
+    private var url: URL
+    private var title: String
 
-    init(path: String) {
-        self.url = URL(fileURLWithPath: path)
+    convenience init(path: String) {
+        let url = URL(fileURLWithPath: path)
+        let title = url.lastPathComponent
+        self.init(url: url, title: title)
+    }
+
+    convenience init(path: String, title: String) {
+        let url = URL(fileURLWithPath: path)
+        self.init(url: url, title: title)
+    }
+
+    init(url: URL, title: String) {
+        self.url = url
+        self.title = title
     }
 
     var previewItemURL: URL! {
@@ -15,14 +28,14 @@ class PreviewItem : NSObject, QLPreviewItem {
     }
 
     var previewItemTitle: String! {
-        return "Preview"
+        return title
     }
 }
 
-class Preview : NSObject, QLPreviewPanelDelegate, QLPreviewPanelDataSource {
-    private let panel: QLPreviewPanel! = QLPreviewPanel.shared()
+class Preview: NSObject, QLPreviewPanelDelegate, QLPreviewPanelDataSource {
+    private let panel: QLPreviewPanel = QLPreviewPanel.shared()
 
-    private var item: PreviewItem!
+    private var item: PreviewItem
 
     init(item: PreviewItem) {
         self.item = item
@@ -46,44 +59,99 @@ class Preview : NSObject, QLPreviewPanelDelegate, QLPreviewPanelDataSource {
     }
 }
 
-class AppDelegate : NSObject, NSApplicationDelegate {
-    private var path: String!
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var args: Args
 
-    init(_ path: String) {
-        self.path = path
+    init(_ args: Args) {
+        self.args = args
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         NSApp.activate(ignoringOtherApps: true)
-        view(path)
+        view(args)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
 
-    func view(_ path: String) {
-        let item = PreviewItem(path: path)
+    func view(_ args: Args) {
+        let item = PreviewItem.from(args: args)
         let preview = Preview(item: item)
         preview.show()
     }
 }
 
+extension PreviewItem {
+    static func from(args: Args) -> PreviewItem {
+        guard let title = args.title else {
+            return PreviewItem(path: args.path)
+        }
+
+        return PreviewItem(path: args.path, title: title)
+    }
+}
+
+enum ArgumentsError: Error {
+    case invalid
+    case empty
+}
+
+class Args {
+    static let usage: String = "Usage: ql [--title <title>] <path>"
+
+    var path: String
+    var title: String?
+
+    init(path: String, title: String?) {
+        self.path = path
+        self.title = title
+    }
+
+    static func from(_ arguments: [String]) throws -> Args {
+        if arguments.count <= 1 {
+            throw ArgumentsError.empty
+        }
+
+        var path: String?
+        var title: String?
+
+        var args = arguments[1..<arguments.endIndex]
+
+        while args.count > 0 {
+            let arg = args.removeFirst()
+
+            if (arg == "--title") {
+                guard args.count > 0 else {
+                    throw ArgumentsError.invalid
+                }
+
+                title = args.removeFirst()
+                continue
+            }
+
+            path = arg;
+        }
+
+        guard path != nil else {
+            throw ArgumentsError.invalid
+        }
+
+        return Args(path: path!, title: title)
+    }
+}
+
 // main
 
-let arguments = CommandLine.arguments
-
-if arguments.count <= 1 {
-    print("usage: ql <path>")
+guard let args = try? Args.from(CommandLine.arguments) else {
+    print(Args.usage)
     exit(1)
 }
 
-let path = arguments[1]
-
 let app = NSApplication.shared
 
-let delegate = AppDelegate(path)
+let delegate = AppDelegate(args)
 app.delegate = delegate
 
 app.run()
